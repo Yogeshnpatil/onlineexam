b1aba7ee93e05f19d0f10b548d9a0481bd7b7875
Yogi <Yogi@Yogi-PC>
2018-07-19 09:00:34 +0530

    after creating online exam project.

---
+++ WEB-INF/classes/com/xpro/onlineexam/authentication/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xpro.onlineexam.authentication.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Yogi
+ */
+@Controller
+@RequestMapping(value = "login.htm")
+public class AuthenticationController
+{
+    static final Logger logger = Logger.getLogger(AuthenticationController.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showLoginPage()
+    {
+        return "login";
+    }
+}
